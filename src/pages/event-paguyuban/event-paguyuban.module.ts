c1b75f07b2ee9e159d06006ece645e71cf875682
Galih <galihkur29@gmail.com>
2020-03-11 17:43:39 +0700

    new page

---
+++ src/pages/event-paguyuban/event-paguyuban.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EventPaguyubanPage } from './event-paguyuban';
+
+@NgModule({
+  declarations: [
+    EventPaguyubanPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(EventPaguyubanPage),
+  ],
+})
+export class EventPaguyubanPageModule {}
