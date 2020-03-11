2ba71a5855e7e3c79981c9f4251fd5ce61da027f
Galih <galihkur29@gmail.com>
2020-03-11 17:45:00 +0700

    anggota reference

---
+++ src/myreference/anggota.ts
@@ -0,0 +1,14 @@
+export interface Anggota {
+    active:Number,
+    alamat:String,
+    create_by:String,
+    email:String,
+    kelamin:String,
+    nama:String,
+    parent:String,
+    phone:String,
+    role:Number,
+    tgl_bergabung:String,
+    tgl_lahir:String,
+    uid:String
+}
