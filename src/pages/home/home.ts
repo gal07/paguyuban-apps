53c4775e68bfdd89be527c1eb3974d0096d32a6d
Galih <galihkur29@gmail.com>
2020-03-13 17:19:25 +0700

    add datepicker native

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DatePicker } from '@ionic-native/date-picker';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public datepicker:DatePicker
+    ) {
+
+     
+
+  }
+
+  async Showdate(){
+
+    this.datepicker.show({
+      date: new Date(),
+      mode: 'date',
+      androidTheme: this.datepicker.ANDROID_THEMES.THEME_HOLO_DARK
+    }).then(
+      date => console.log('Got date: ', date),
+      err => console.log('Error occurred while getting date: ', err)
+    );
+    
 
   }
 
